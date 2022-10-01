790bce4e4093a793f1e9c2555a25eb5a4806495f
parker <parker.nalchajian@gmail.com>
2022-10-01 15:41:42 -0700

    add dash

--- Atoms/Player/PlayerController.cs
+++ Atoms/Player/PlayerController.cs
@@ -16,12 +16,20 @@ public class PlayerController : KinematicBody2D
 	private float _decelerationFactor;
 	private float _topSpeed;
 	private float _topSpeedSquared;
+	
+	private bool _isDashing;
+	[Export]public float dashTime;
+	[Export]public float dashMultiplier;
+	
+	private Sprite _sprite;
 		
 	public override void _Ready()
 	{
+		_sprite = (Sprite)GetNode("Sprite");
 		_velocity = new Vector2(0, 0);
 		_inputDirection = new Vector2(0, 0);
 		_isRunning = false;
+		_isDashing = false;
 		Assert.True(timeToAccelerate > 0, "acceleration time must be nonzero");
 		Assert.True(timeToDecelerate > 0, "deceleration tme must be nonzero");
 		_topSpeed = tilesPerSecond * tileSize;
@@ -35,10 +43,29 @@ public class PlayerController : KinematicBody2D
 		_inputDirection.x = Input.GetActionStrength("move_right") - Input.GetActionStrength("move_left");
 		_inputDirection.y = Input.GetActionStrength("move_down") - Input.GetActionStrength("move_up");
 	}
+
+	public override void _Input(InputEvent e)
+	{
+		if (e.IsActionPressed("dash"))
+		{
+//			InputEventAction a = e as InputEventAction;
+//			GD.Print(a);
+//			if (a.Action == "dash" && a.Pressed)
+//			{
+			Dash();
+//			}
+		}
+	}
 	
 	public override void _PhysicsProcess(float delta) {
 		int runModifier = _isRunning ? 1 : 2;
-		_velocity = MoveAndSlide(ApplyForces(delta, _inputDirection));
+		if (_isDashing)
+		{
+			_velocity = MoveAndSlide(_velocity);
+		} else
+		{
+			_velocity = MoveAndSlide(ApplyForces(delta, _inputDirection));
+		}
 	}
 	
 	public Vector2 ApplyForces(float delta, Vector2 inputDirection) {
@@ -67,5 +94,18 @@ public class PlayerController : KinematicBody2D
 			}
 		}
 		return v;
+	}
+	
+	async public void Dash()
+	{
+		if (_isDashing) return;
+		_velocity = _inputDirection.Normalized() * _topSpeed * dashMultiplier;
+		_isDashing = true;
+		_sprite.SelfModulate = new Color(1, 1, 1, 0.5f);
+		// TODO: disable then enable relevant area2Ds that handle interaction with
+		// hazards, missiles, coins, etc. as needed
+		await ToSignal(GetTree().CreateTimer(dashTime), "timeout");
+		_isDashing = false;
+		_sprite.SelfModulate = new Color(1, 1, 1, 1);
 	}
 }
