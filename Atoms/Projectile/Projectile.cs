2ba2755f98d723b6ad1337f50d9f1cb2b2b15add
Kevin Thompson <kevin.thompson@agrian.com>
2022-10-01 00:24:16 -0700

    Add basic projectile (uses MainScene as demo for now)

---
+++ Atoms/Projectile/Projectile.cs
@@ -0,0 +1,75 @@
+using Godot;
+using JamToolkit.Util;
+
+public class Projectile : Area2D
+{
+	[Export] public float Speed { get; set; } = 350;
+	[Export] public float SteerForce { get; set; } = 50;
+
+	Vector2 _velocity = Vector2.Zero;
+	Vector2 _acceleration = Vector2.Zero;
+
+	Node2D _target;
+	private Timer _timer;
+
+	public void Start(Transform2D transform, Node2D target)
+	{
+		GlobalTransform = transform;
+		Rotation += (float)GD.RandRange(-0.09f, 0.09f);
+		_velocity = transform.x * Speed;
+		_target = target;
+		_timer.Start(10);
+	}
+
+	public override void _Ready()
+	{
+		_timer = this.FindChildByName<Timer>("Lifetime");
+
+		Connect("body_entered", this, nameof(OnProjectileBodyEntered));
+		_timer.Connect("timeout", this, nameof(OnLifetimeTimeout));
+		_timer.Connect("tree_exiting", this, nameof(OnLifetimeExitingTree));
+	}
+
+	protected override void Dispose(bool disposing)
+	{
+		Disconnect("body_entered", this, nameof(OnProjectileBodyEntered));
+
+		base.Dispose(disposing);
+	}
+
+	Vector2 Seek()
+	{
+		if (_target == null) return Vector2.Zero;
+
+		// desired vector
+		var desired = (_target.Position - Position).Normalized() * Speed;
+
+		// minus current velocity vector to get our course correction
+		return (desired - _velocity).Normalized() * SteerForce;
+	}
+
+	public void OnProjectileBodyEntered(Node body)
+	{
+		// TODO: explode projectile, kill player
+		QueueFree();
+	}
+
+	void OnLifetimeExitingTree()
+	{
+		_timer.Disconnect("timeout", this, nameof(OnLifetimeTimeout));
+		_timer.Disconnect("tree_exiting", this, nameof(OnLifetimeExitingTree));
+	}
+
+	void OnLifetimeTimeout() => QueueFree();
+
+	public override void _PhysicsProcess(float delta)
+	{
+		// update acc vector to home in on target
+		_acceleration += Seek();
+		// regular physics calculation
+		_velocity += _acceleration * delta;
+		_velocity = _velocity.LimitLength(Speed);
+		Rotation = _velocity.Angle();
+		Position += _velocity * delta;
+	}
+}
---
+++ Scenes/MainScene.cs
@@ -0,0 +1,33 @@
+using Godot;
+using System;
+using JamToolkit.Util;
+
+/// <summary>
+/// TODO: this just sets up a "Player" that will be replaced with the real player and creates missiles every 10s
+/// </summary>
+public class MainScene : Node2D
+{
+	private float time = 0;
+	private PackedScene _projectile;
+	private Player _player;
+
+
+	public override void _Ready()
+	{
+		_projectile = ResourceLoader.Load<PackedScene>("res://Atoms/Projectile/Projectile.tscn");
+		_player = this.FindSingleton<Player>();
+	}
+
+	public override void _Process(float delta)
+	{
+		time += delta;
+
+		if (time > 10)
+		{
+			time -= 10;
+			var bullet = _projectile.Instance<Projectile>();
+			this.AddChild(bullet);
+			bullet.Start(this.Transform, _player);
+		}
+	}
+}
