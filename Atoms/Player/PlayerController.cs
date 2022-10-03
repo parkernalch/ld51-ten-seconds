10ed3650bc1b6e4cf056e63b574b7ac0be719abb
Kevin Thompson <kevin@theautomaters.com>
2022-10-03 00:02:30 -0700

    Update player to track health, get hurt and die on missile collisions

--- Atoms/Player/PlayerController.cs
+++ Atoms/Player/PlayerController.cs
@@ -4,19 +4,24 @@ using JamToolkit.Util;
 
 public class PlayerController : KinematicBody2D
 {
+	EventBus _eventBus;
 	private Vector2 _inputDirection;
-	private bool _isRunning;
+
 	private AnimationNodeStateMachinePlayback _stateMachine;
 	private Vector2 _velocity;
 	[Export] public int tileSize;
 	[Export] public int tilesPerSecond;
 	[Export] public float timeToAccelerate;
 	[Export] public float timeToDecelerate;
+	[Export] public int Health { get; set; } = 10;
+
+	private int _lastHealth;
 
 	private float _accelerationFactor;
 	private float _decelerationFactor;
 	private float _topSpeed;
 	private float _topSpeedSquared;
+	private float _hurtDuration;
 
 	private bool _isDashing;
 	private bool _canDash;
@@ -26,13 +31,18 @@ public class PlayerController : KinematicBody2D
 	[Export] public float dashMultiplier;
 
 	private Sprite _sprite;
+	private AnimationTree _animationTree;
 
 	public override void _Ready()
 	{
-		_sprite = (Sprite)GetNode("Sprite");
+		_eventBus = GetNode<EventBus>("/root/EventBus");
+		_animationTree = this.FindChild<AnimationTree>();
+		_stateMachine = (AnimationNodeStateMachinePlayback)_animationTree.Get("parameters/playback");
+
+		_sprite = this.FindChild<Sprite>();
+		_lastHealth = Health;
 		_velocity = new Vector2(0, 0);
 		_inputDirection = new Vector2(0, 0);
-		_isRunning = false;
 		_isDashing = false;
 		_canDash = true;
 		Assert.True(timeToAccelerate > 0, "acceleration time must be nonzero");
@@ -40,17 +50,30 @@ public class PlayerController : KinematicBody2D
 		_topSpeed = tilesPerSecond * tileSize;
 		_topSpeedSquared = Mathf.Pow(_topSpeed, 2);
 		_accelerationFactor = _topSpeed / (timeToAccelerate * timeToAccelerate);
-		_decelerationFactor = _topSpeed / (timeToDecelerate);
+		_decelerationFactor = _topSpeed / timeToDecelerate;
+
+		_eventBus.Connect(nameof(EventBus.MissileConnected), this, nameof(OnMissileHit));
 	}
 
-	public override void _Process(float delta) {
-		_isRunning = Input.IsActionPressed("run");
+	public override void _Process(float delta)
+	{
+		Input.IsActionPressed("run");
+
+		if (IsDead) return;
 		_inputDirection.x = Input.GetActionStrength("move_right") - Input.GetActionStrength("move_left");
 		_inputDirection.y = Input.GetActionStrength("move_down") - Input.GetActionStrength("move_up");
 	}
 
 	public override void _Input(InputEvent e)
 	{
+		if (e is InputEventKey key && key.Scancode == (int)KeyList.Escape)
+		{
+			GetTree().Quit();
+		}
+
+		if (IsDead) return;
+
+
 		if (e.IsActionPressed("dash"))
 		{
 			//			InputEventAction a = e as InputEventAction;
@@ -62,40 +85,53 @@ public class PlayerController : KinematicBody2D
 		}
 	}
 
-	public override void _PhysicsProcess(float delta) {
-		int runModifier = _isRunning ? 1 : 2;
-		AnimatePlayer();
-		if (_isDashing)
+	public override void _PhysicsProcess(float delta)
+	{
+		if (IsDead)
 		{
-			_velocity = MoveAndSlide(_velocity);
-		} else
-		{
-			_velocity = MoveAndSlide(ApplyForces(delta, _inputDirection));
+			_stateMachine.Travel("Die");
+			return;
 		}
-	}
-	
-	public void AnimatePlayer() {
-		var _animationTree = (AnimationTree)GetNode("AnimationTree");
-		_stateMachine = (AnimationNodeStateMachinePlayback)_animationTree.Get("parameters/playback");
-		if (_velocity.LengthSquared() == 0)
+
+		var dashVelocity = _isDashing ? _velocity : ApplyForces(delta, _inputDirection);
+		_velocity = MoveAndSlide(dashVelocity);
+
+		if (IsHurt)
 		{
-			_stateMachine.Travel("Idle");
-		} else
-		{
-			if (_isDashing)
+			_stateMachine.Travel("Hurt");
+			_hurtDuration += delta;
+			if (_hurtDuration > 0.25)
 			{
-				_stateMachine.Travel("Dash");	
-			} else {
-				_stateMachine.Travel("Run");	
+				_hurtDuration = 0;
+				_lastHealth = Health;
 			}
 		}
-		_animationTree.Set("parameters/Idle/blend_position", _velocity);
-		_animationTree.Set("parameters/Run/blend_position", _velocity);
+		else if (_isDashing)
+		{
+			_stateMachine.Travel("Dash");
+		}
+		else if (IsRunning)
+		{
+			_stateMachine.Travel("Run");;
+			_animationTree.Set("parameters/Run/blend_position", _velocity);
+		}
+		else
+		{
+			_stateMachine.Travel("Idle");
+			_animationTree.Set("parameters/Idle/blend_position", _velocity);
+		}
 	}
 
-	public Vector2 ApplyForces(float delta, Vector2 inputDirection) {
-		Vector2 v = _velocity;
-		float v2 = _velocity.LengthSquared();
+	private bool IsHurt => Health != _lastHealth;
+
+	private bool IsRunning => _velocity.LengthSquared() > 0;
+
+	private bool IsDead => Health == 0;
+
+
+	Vector2 ApplyForces(float delta, Vector2 inputDirection)
+	{
+		var v = _velocity;
 		if (_inputDirection.LengthSquared() == 0)
 		{
 			// Decelerate
@@ -103,25 +139,29 @@ public class PlayerController : KinematicBody2D
 			if (
 				_velocity.Dot(v) < 0 ||
 				v.LengthSquared() <= _topSpeedSquared * 0.1
-			) {
-				return v * 0;
+			)
+			{
+				return Vector2.Zero;
 			}
-		} else
+		}
+		else
 		{
 			// accelerate
 			v += inputDirection.Normalized() * _accelerationFactor * delta;
 			if (
 				_velocity.LengthSquared() >= _topSpeedSquared * 0.95f ||
 				v.LengthSquared() >= _topSpeedSquared * 0.95f
-			) {
+			)
+			{
 				// Allow for full control at or around top speed
 				return inputDirection.Normalized() * _topSpeed;
 			}
 		}
+
 		return v;
 	}
 
-	async public void Dash()
+	async void Dash()
 	{
 		if (_isDashing || !_canDash) return;
 		_velocity = _inputDirection.Normalized() * _topSpeed * dashMultiplier;
@@ -129,11 +169,24 @@ public class PlayerController : KinematicBody2D
 		_sprite.SelfModulate = new Color(1, 1, 1, 0.5f);
 		// TODO: disable then enable relevant area2Ds that handle interaction with
 		// hazards, missiles, coins, etc. as needed
-		await ToSignal(GetTree().CreateTimer(dashTime), "timeout");
+		await Delay(dashTime);
 		_isDashing = false;
-		_sprite.SelfModulate = new Color(1, 1, 1, 1);
+		_sprite.SelfModulate = new Color(1, 1, 1);
 		_canDash = false;
-		await ToSignal(GetTree().CreateTimer(dashCooldown), "timeout");
+		await Delay(dashCooldown);
 		_canDash = true;
+	}
+
+	private SignalAwaiter Delay(float timeSec) => ToSignal(GetTree().CreateTimer(timeSec), "timeout");
+
+	void OnMissileHit(Projectile projectile)
+	{
+		if (Health == 0) return;
+
+		Health--;
+		if (Health == 0)
+		{
+			_velocity = Vector2.Zero;
+		}
 	}
 }
